90cc46478751333689e5016948e77f690aaabd0d
AngryFist <go3625@gmail.com>
2018-09-30 12:32:13 +0800

    Update main.js

--- test/requirejs/main.js
+++ test/requirejs/main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl : './',
     paths: {
-        'jquery' : 'http://activity.daojia.com.cn/dj_static/jquery/1.12.3/jquery.min'
+        'jquery' : '../dj_static/jquery/1.12.3/jquery.min'
     },
     shim: {},
     deps : ['jquery']
